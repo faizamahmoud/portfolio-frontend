26cc1f0a5aa9be7f9cdf738a5cd1921f4e6e427c
faizamahmoud <faiza.mahmoud@email.com>
2022-11-01 15:33:00 -0600

    deploy

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+
+
+function Home() {
+
+  return (
+    <header>
+      <h1>My Portfolio Page</h1>
+     
+    </header>
+  );
+}
+
+export default Home;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,20 +1,41 @@
 // import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid";
 import React from "react";
-
+import images from '../myimages/two.jpeg'
 
 export default function Skills() {
   return (
-    <section id="skills" >
-      <div className="container bg-gray px-5 py-10 mx-auto">
+    <div className='bio'>
+    <div id="about">
+    <div className="container mx-auto flex px-5 py-2 md:flex-row flex-col items-center">
+      <div className="lg:flex-grow ">
+        <p className="intro"  style={{transitionDelay: '1ms', font:'16px'}}>
+          Hey, My name is 
+          <br style={{color:'#708090'}} className="hidden lg:inline-block" />I love to build amazing
+          apps.
+        </p>
+        <p className="mb-8 text-white leading-relaxed">
+        I’m a Full stack engineer with a background in Math and Finance. I prioritize creating shared success, personal growth while feeding my continuous curiosity in logic-based fields such as computer science. I’m a life-long learner focused on solving real-world problems.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        
+      </div>
+      <div>
+        <img className="profile-img"
+          src={images}
+          alt="me"
+        />
+      </div>
+    </div>
+  </div>
+
+    <div id="skills" >
+      <div className="container px-5 py-10 mx-auto">
         <div className="text-center mb-20">
           {/* <ChipIcon className="w-10 inline-block mb-4" /> */}
-          <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
-            Skills &amp; Technologies
-          </h1>
-         
+          <h3 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+            My tool box
+          </h3>
         </div>
-        <div>
-
           <div className='row'>
             <div className='col s4'>
               <h5 class="white-text">LANGUAGES</h5>
@@ -72,8 +93,22 @@ export default function Skills() {
             </div>
           </div>
         </div>
-        <div class="row center"></div>
+        
       </div>
-    </section>
+      {/* <div className="link-text">
+          <a
+            href="/contact"
+            id="text">
+            Work With Me
+          </a>
+          <a
+            href="/projects"
+            id="text">
+            See My Past Work
+          </a>
+        </div> */}
+    </div>
+    
+    
   );
 }
